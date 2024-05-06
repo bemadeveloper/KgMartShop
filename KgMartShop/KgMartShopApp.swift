43db8da25d12c5664680979487b8adc013c7b8d3
bemadeveloper <akbermet1990@icloud.com>
2024-05-06 18:36:44 +0600

    Initial Commit

---
+++ KgMartShop/KgMartShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  KgMartShopApp.swift
+//  KgMartShop
+//
+//  Created by Bema on 6/5/24.
+//
+
+import SwiftUI
+
+@main
+struct KgMartShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
